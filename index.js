f868e0e4663d8a2129f9c1c456fc2f6f08d19d04
Peter Bacenet <peterbacenet@icloud.com>
2021-08-17 16:36:19 -0400

    commit tree b7f215423a7e10829dd58c0a00acd03dfda78d7b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",() => {
+let changes = document.getElementById('text');
+changes.textContent = 'This is really cool!'
+});
+  
+//   console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered"); 
+  
+  // Your code goes here
